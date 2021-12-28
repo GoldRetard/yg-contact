1dca6c3845875b3c47356d7dd999670b043d63eb
Alexey Natalushko <91388240+GoldRetard@users.noreply.github.com>
2021-12-28 23:39:54 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+				<meta charset="UTF-8">
+				<title>Заголовок страницы</title>
+</head>
+<body>
+	<from action="mail.php" method="POST">
+	  <selection name="title">
+	    <option disabled selected>-</option>
+	    <option value="1">Помощь и поддержка</option>
+	    <option value="2">Trust & Saftey</option>
+	    <option value="3">Сообщение о неисправностях</option>
+	    <option value="4">Поддержка разработчиков</option>
+	  </selection>
+	  
+	  <input type="email" name="email" placeholder="Введите адрес электронной почты" required>
+	  
+	  <textarea name="msg" placeholder="Введите сообщение службе поддержки" maxlength="2000" required>
+	  </textarea>
+	  
+	  <input type="send" value="Отправить">
+	</from>
+	
+</body>
+</html>
