f72b8c65eea2f86710f9187541af219af4746032
Alexey Natalushko <91388240+GoldRetard@users.noreply.github.com>
2021-12-31 15:22:57 +0200

    update login.php

--- login.php
+++ login.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+				<meta charset="UTF-8">
+				<link rel="stylesheet" href="/css/style.css"
+				<title>YouGro</title>
+</head>
+<body>
+				<form action="" method="">
+				  <label>ЛОГИН</label>
+				  <input type="text">
+				  <label>ПАРОЛЬ</label>
+				  <input type="password">
+				  <button>Войти</button>
+				</form>
+</body>
+</html>
 
